a3ca7400eb7454ae20035442a3f610d856b8b455
Dzmitry <gera.1212@yandex.ru>
2023-07-16 19:55:52 +0300

    добавляет исправления csscomb и блок player

--- source/js/modules/player.js
+++ source/js/modules/player.js
@@ -1,12 +1,12 @@
 const isButtonPlayVideo = () => {
-  const root = document.querySelector('.description__player');
+  const root = document.querySelector('.player');
   const button = root.querySelector('button');
   const iframe = `
   <iframe width="100%" height="100%" src="${button.dataset.src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   `;
 
   button.addEventListener('click', () => {
-    root.classList.add('description__player--play-video');
+    root.classList.add('player--play-video');
     root.innerHTML = iframe;
   }, {once: true});
 };
