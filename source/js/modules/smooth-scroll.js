3bc1520123e1ec3fda9fa541abc7f558995d36c6
Dzmitry <gera.1212@yandex.ru>
2023-07-16 10:17:44 +0300

    добавляет плавный скролл по якорным ссылкам

---
+++ source/js/modules/smooth-scroll.js
@@ -0,0 +1,17 @@
+const smoothScroll = () => {
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  anchors.forEach((anchor) => {
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute('href').substring(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+};
+
+export {smoothScroll};
