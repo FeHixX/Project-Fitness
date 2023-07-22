e4c744e55a5a91ee78c3b6a8a7e02738efca4869
Dzmitry <gera.1212@yandex.ru>
2023-07-22 23:15:31 +0300

    добавляет исправления

--- source/js/modules/trainers/slider.js
+++ source/js/modules/trainers/slider.js
@@ -10,11 +10,23 @@ const initSliderTrainers = () => {
         slidesPerView: 1,
         spaceBetween: 20,
       },
+      590: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
       768: {
         slidesPerView: 2,
         spaceBetween: 30,
       },
+      995: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
       1200: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+      1310: {
         slidesPerView: 4,
         spaceBetween: 40,
       },
